68e56c264d1fa33e8112f143b8152e4ac71187ac
Isaac Talb <thaungahlaybaw@gmail.com>
2024-12-28 02:40:48 +0700

    update for the learning-panel/blog-post/index.*

--- resources/views/admin/learning-panel/blog-post/index.blade.php
+++ resources/views/admin/learning-panel/blog-post/index.blade.php
@@ -7,10 +7,44 @@
         <h1 class="text-2xl font-bold mb-4">Learning Resources for Blog Post Creation</h1>
         <p class="mb-6 text-gray-600">
             This guide will help you create blog posts using HTML and Tailwind CSS. You'll learn about responsive layouts, HTML elements, and Tailwind's grid and flex utilities for content cards.
+        <i class="text-sm text-gray-500">Note: This guide is no need of a basic understanding of HTML and Tailwind CSS. You can use several AI tools to generate HTML and Tailwind CSS code.</i>
+        <br>
+        <span class="text-red-500 text-md mt-4">Please: Click the image to view clearly and double click to zoom in/out.</span>
+
         </p>
 
-        <div class="mb-6">
-
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="flex flex-col items-center p-4 border border-gray-300 rounded-lg">
+                <a href="{{ asset('assets/img/learning-panel/blog/learn-blog1.png') }}" data-lightbox="image-1" data-title="Step 1: Click &quot;Create Blog Post&quot; and give title.">
+                    <img src="{{ asset('assets/img/learning-panel/blog/learn-blog1.png') }}" alt="step 1" class="w-full h-auto mb-4">
+                </a>
+                <span class="text-base md:text-lg lg:text-xl xl:text-2xl bg-yellow-100 p-2 rounded">Step 1: Click "Create Blog Post" and give title.</span>
+            </div>
+            <div class="flex flex-col items-center p-4 border border-gray-300 rounded-lg">
+                <a href="{{ asset('assets/img/learning-panel/blog/learn-blog2.png') }}" data-lightbox="image-2" data-title="Step 2: Go to any AI free chat and type &quot;Create a content for blog post based on my desire content blog post by using HTML, Tailwind and copy to content section.&quot;">
+                    <img src="{{ asset('assets/img/learning-panel/blog/learn-blog2.png') }}" alt="step 2" class="w-full h-auto mb-4">
+                </a>
+                <span class="text-base md:text-lg lg:text-xl xl:text-2xl bg-green-100 p-2 rounded">Step 2: Go to any AI free chat and type "Create a content for blog post based on my desire content blog post by using HTML, Tailwind and copy to content section."</span>
+            </div>
+            <div class="flex flex-col items-center p-4 border border-gray-300 rounded-lg">
+                <a href="{{ asset('assets/img/learning-panel/blog/learn-blog3.png') }}" data-lightbox="image-3" data-title="Step 3: Upload multiple images & video embedded are optional and still you can use photo or video embedded in the content section.">
+                    <img src="{{ asset('assets/img/learning-panel/blog/learn-blog3.png') }}" alt="step 3" class="w-full h-auto mb-4">
+                </a>
+                <span class="text-base md:text-lg lg:text-xl xl:text-2xl bg-blue-100 p-2 rounded">Step 3: Upload multiple images & video embedded are optional and still you can use photo or video embedded in the content section.</span>
+            </div>
+            <div class="flex flex-col items-center p-4 border border-gray-300 rounded-lg">
+                <a href="{{ asset('assets/img/learning-panel/blog/learn-blog4.png') }}" data-lightbox="image-4" data-title="Step 4: Give a slug name with your desire and it will be appear in behnid of the domain/blog/slug-name.">
+                    <img src="{{ asset('assets/img/learning-panel/blog/learn-blog4.png') }}" alt="step 4" class="w-full h-auto mb-4">
+                </a>
+                <span class="text-base md:text-lg lg:text-xl xl:text-2xl bg-orange-100 p-2 rounded">Step 4: Give a slug name with your desire and it will be appear in behnid of the domain/blog/slug-name.</span>
+            </div>
+            <div class="flex flex-col items-center p-4 border border-gray-300 rounded-lg">
+                <a href="{{ asset('assets/img/learning-panel/blog/learn-blog5.png') }}" data-lightbox="image-5" data-title="Step 5: Give an author name based on who created the blog post and click create and then boommm it's all finished.">
+                    <img src="{{ asset('assets/img/learning-panel/blog/learn-blog5.png') }}" alt="step 5" class="w-full h-auto mb-4">
+                </a>
+                <span class="text-base md:text-lg lg:text-xl xl:text-2xl bg-red-100 p-2 rounded">Step 5: Give an author name based on who created the blog post and click create and then boommm it's all finished.</span>
+            </div>
         </div>
     </div>
 @endsection
+
--- resources/views/admin/learning-panel/index.blade.php
+++ resources/views/admin/learning-panel/index.blade.php
@@ -11,13 +11,13 @@
 
         <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
             <!-- Blog Card -->
-            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow">
+            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow h-96 md:h-128 lg:h-160">
                 <div class="flex-1 overflow-hidden">
                     <img src="{{ asset('assets/img/learning-panel/blog/learn-blog1.png') }}" class="object-cover w-full h-full" alt="">
                 </div>
                 <div class="absolute inset-0 z-10 bg-gradient-to-t from-black"></div>
                 <div class="absolute inset-x-0 bottom-0 z-20 p-4">
-                    <div class="bg-gray-900/50 rounded-lg p-4">
+                    <div class="bg-sky-950/75 rounded-lg p-4">
                         <p class="mb-1 text-sm text-white text-opacity-80">Isaac Talb • <time>18 Nov 2022</time></p>
                         <h3 class="text-xl font-medium text-white">Learn Blog Post Creation</h3>
                         <ul class="list-disc pl-6 mt-2">
@@ -26,20 +26,19 @@
                             <li class="text-gray-300">Design content cards with Tailwind's grid and flex utilities.</li>
                         </ul>
                     </div>
-                    
                     <div class="mt-6 flex justify-center">
-                        <a href="{{ route('admin.learning-panel.blog-post.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-gray-100 transition-colors duration-300">Learn More</a>
+                        <a href="{{ route('admin.learning-panel.blog-post.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-sky-100 transition-colors duration-300">Learn More</a>
                     </div>
                 </div>
             </div>
             <!-- Home Page Card -->
-            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow">
+            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow h-96 md:h-128 lg:h-160">
                 <div class="flex-1 overflow-hidden">
                     <img src="{{ asset('assets/img/learning-panel/home-page/homepage-post4.png') }}" class="object-cover w-full h-full" alt="" />
                 </div>
                 <div class="absolute inset-0 z-10 bg-gradient-to-t from-black"></div>
                 <div class="absolute inset-x-0 bottom-0 z-20 p-4">
-                    <div class="bg-gray-900/50 rounded-lg p-4">
+                    <div class="bg-sky-950/75 rounded-lg p-4">
                         <p class="mb-1 text-sm text-white text-opacity-80">Isaac Talb • <time>18 Nov 2022</time></p>
                         <h3 class="text-xl font-medium text-white">Learn Home Page Creation</h3>
                         <ul class="list-disc pl-6 mt-2">
@@ -49,18 +48,18 @@
                         </ul>
                     </div>
                     <div class="mt-6 flex justify-center">
-                        <a href="{{ route('admin.learning-panel.home-page.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-gray-100 transition-colors duration-300">Learn More</a>
+                        <a href="{{ route('admin.learning-panel.home-page.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-sky-100 transition-colors duration-300">Learn More</a>
                     </div>
                 </div>
             </div>
             <!-- About Page Card -->
-            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow">
+            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow h-96 md:h-128 lg:h-160">
                 <div class="flex-1 overflow-hidden">
                     <img src="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" class="object-cover w-full h-full" alt="" />
                 </div>
                 <div class="absolute inset-0 z-10 bg-gradient-to-t from-black"></div>
                 <div class="absolute inset-x-0 bottom-0 z-20 p-4">
-                    <div class="bg-gray-900/50 rounded-lg p-4">
+                    <div class="bg-sky-950/75 rounded-lg p-4">
                         <p class="mb-1 text-sm text-white text-opacity-80">Isaac Talb • <time>18 Nov 2022</time></p>
                         <h3 class="text-xl font-medium text-white">Learn About Page Creation</h3>
                         <ul class="list-disc pl-6 mt-2">
@@ -70,18 +69,18 @@
                         </ul>
                     </div>
                     <div class="mt-6 flex justify-center">
-                        <a href="{{ route('admin.learning-panel.about-page.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-gray-100 transition-colors duration-300">Learn More</a>
+                        <a href="{{ route('admin.learning-panel.about-page.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-sky-100 transition-colors duration-300">Learn More</a>
                     </div>
                 </div>
             </div>
             <!-- Contact Message Card -->
-            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow">
+            <div class="relative flex flex-col items-stretch bg-white rounded-lg shadow h-96 md:h-128 lg:h-160">
                 <div class="flex-1 overflow-hidden">
                     <img src="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" class="object-cover w-full h-full" alt="" />
                 </div>
                 <div class="absolute inset-0 z-10 bg-gradient-to-t from-black"></div>
                 <div class="absolute inset-x-0 bottom-0 z-20 p-4">
-                    <div class="bg-gray-900/50 rounded-lg p-4">
+                    <div class="bg-sky-950/75 rounded-lg p-4">
                         <p class="mb-1 text-sm text-white text-opacity-80">Isaac Talb • <time>18 Nov 2022</time></p>
                         <h3 class="text-xl font-medium text-white">How to view Contact Messages</h3>
                         <ul class="list-disc pl-6 mt-2">
@@ -89,10 +88,11 @@
                         </ul>
                     </div>
                     <div class="mt-6 flex justify-center">
-                        <a href="{{ route('admin.learning-panel.contact.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-gray-100 transition-colors duration-300">Learn More</a>
+                        <a href="{{ route('admin.learning-panel.contact.index') }}" class="inline-block bg-white text-gray-700 px-4 py-2 rounded-md hover:bg-sky-100 transition-colors duration-300">Learn More</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
