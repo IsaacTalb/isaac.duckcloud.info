c28a1a1c3f2f31742e93237c1c23e41b6bf79464
Isaac Talb <thaungahlaybaw@gmail.com>
2025-01-10 10:30:05 +0700

    added live bot

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -23,6 +23,20 @@
     </script>
     <!-- amp ads end -->
     @yield('styles')
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/678090fcaf5bfec1dbe9895c/1ih73cnkj';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script--> 
 </head>
 <body class="font-sans antialiased bg-gray-100">
     <header class="bg-gray-800 text-white p-4">
